ad2c53aca5c7455a7a0271b1fb4412b2ae88aa85
Camille  Dickens <camilledickens@Camilles-MacBook-Pro.local>
2025-10-04 11:50:24 -0400

    finish slot

--- SlotMachine/Program.cs
+++ SlotMachine/Program.cs
@@ -5,6 +5,7 @@ using System;
 using System.ComponentModel;
 using System.Data;
 using System.Net;
+using System.Net.NetworkInformation;
 
 namespace MyApp
 {
@@ -13,7 +14,6 @@ namespace MyApp
         static void Main(string[] args)
         {
             bool bigLoop = false;
-            for (int o = 0; o < 3; o++)
             {
                 Random rng = new Random();
                 const int LOWER_NUMBER = 3;
@@ -23,143 +23,168 @@ namespace MyApp
                 const int LOW_NUMBER = 1;
                 const int HIGH_NUMBER = 4;
                 const string RUN_AGAIN = "Y";
+                bool runAgain = true;
                 
 
                 const int HORIZONTAL_MODE = 1;
                 const int VERTICAL_MODE = 2;
                 const int DIAGONAL_MODE = 3;
+                const int WIN_VAR = 10;
                 
-                
-                int[,] slots = new int[ROWS, COLUMNS];
-
-                Console.WriteLine("Please play the Slot Machine");
-                Console.WriteLine($"Press {HORIZONTAL_MODE} for horizontal win");
-                Console.WriteLine($"Press {VERTICAL_MODE} for vertical win");
-                Console.WriteLine($"Press {DIAGONAL_MODE} for diagonal win");
-                int choice = int.Parse(Console.ReadLine());
-
-
+                Console.WriteLine("How much money do you want to enter?");
+                Console.Write("Enter Money:");
+                int Money = int.Parse(Console.ReadLine());
+                while (Money < 0)
                 {
-                    if (choice != HORIZONTAL_MODE && choice != VERTICAL_MODE && choice != DIAGONAL_MODE)
-                        Console.WriteLine("Do user input until choice is valid.");
+                    Console.Write("Please Enter At Least One Dollar:"); 
+                    Money = int.Parse(Console.ReadLine());
+                }
+
+                while (Money > 0 && runAgain == true)
+                {
+
+                    int[,] slots = new int[ROWS, COLUMNS];
+
+                    Console.WriteLine("Please play the Slot Machine");
+                    Console.WriteLine($"Press {HORIZONTAL_MODE} for horizontal win");
+                    Console.WriteLine($"Press {VERTICAL_MODE} for vertical win");
+                    Console.WriteLine($"Press {DIAGONAL_MODE} for diagonal win");
+                    int choice = int.Parse(Console.ReadLine());
+
                     {
+                        if (choice != HORIZONTAL_MODE && choice != VERTICAL_MODE && choice != DIAGONAL_MODE)
+                        {
+                            Console.WriteLine("Do user input until choice is valid.");
+                            //TODO stop the program
+                        }
+
+
+                        for (int i = 0; i < ROWS; i++)
+                        {
+                            for (int j = 0; j < COLUMNS; j++)
+                            {
+                                slots[i, j] = rng.Next(LOW_NUMBER, HIGH_NUMBER);
+                            }
+                        }
+
+                        Console.WriteLine("\nSlot Machine:");
+                        for (int i = 0; i < ROWS; i++)
+                        {
+                            for (int j = 0; j < COLUMNS; j++)
+                            {
+                                Console.Write(slots[i, j].ToString().PadLeft(4));
+                            }
+
+                            Console.WriteLine();
+                        }
+
+
+                        bool isWin = false;
+                        Money = Money - 5;
+
+                        if (choice == HORIZONTAL_MODE)
 
                         {
                             for (int i = 0; i < ROWS; i++)
                             {
-                                for (int j = 0; j < COLUMNS; j++)
+                                bool rowWin = true;
+                                for (int j = 1; j < COLUMNS; j++)
                                 {
-                                    slots[i, j] = rng.Next(LOW_NUMBER, HIGH_NUMBER);
-                                }
-                            }
-
-                            Console.WriteLine("\nSlot Machine:");
-                            for (int i = 0; i < ROWS; i++)
-                            {
-                                for (int j = 0; j < COLUMNS; j++)
-                                {
-                                    Console.Write(slots[i, j].ToString().PadLeft(4));
-                                }
-
-                                Console.WriteLine();
-                            }
-
-
-                            bool isWin = false;
-                            
-                            if (choice == HORIZONTAL_MODE)
-
-                            {
-                                for (int i = 0; i < ROWS; i++)
-                                {
-                                    bool rowWin = true;
-                                    for (int j = 1; j < COLUMNS; j++)
+                                    if (slots[i, j] != slots[i, 0])
                                     {
-                                        if (slots[i, j] != slots[i, 0])
-                                        {
-                                            rowWin = false;
-                                            break;
-                                        }
-                                    }
-
-
-                                    if (rowWin)
-                                    {
-                                        isWin = true;
-                                        break;
-                                    }
-                                }
-                            }
-                            else if (choice == VERTICAL_MODE)
-                            {
-                                for (int j = 0; j < COLUMNS; j++)
-                                {
-                                    bool colWin = true;
-                                    for (int i = 1; i < ROWS; i++)
-                                    {
-                                        if (slots[i, j] != slots[0, j])
-                                        {
-                                            colWin = false;
-                                            break;
-                                        }
-                                    }
-
-                                    if (colWin)
-                                    {
-                                        isWin = true;
-                                        break;
-                                    }
-                                }
-                            }
-                            else if (choice == DIAGONAL_MODE)
-                            {
-                                bool mainDiagonalWin = true;
-                                for (int i = 1; i < ROWS; i++)
-                                {
-                                    if (slots[i, i] != slots[0, 0])
-                                    {
-                                        mainDiagonalWin = false;
+                                        rowWin = false;
                                         break;
                                     }
                                 }
 
-                                bool antiDiagonalWin = true;
-                                for (int i = 1; i < ROWS; i++)
-                                {
-                                    if (slots[i, COLUMNS - 1 - i] != slots[0, COLUMNS - 1])
-                                    {
-                                        antiDiagonalWin = false;
-                                        break;
-                                    }
-                                }
 
-                                if (mainDiagonalWin || antiDiagonalWin)
+                                if (rowWin)
                                 {
                                     isWin = true;
-                                    Console.WriteLine("🎉 Congratulations, you win!");
+                                    break;
+                                }
+                            }
+                        }
+                        else if (choice == VERTICAL_MODE)
+                        {
+                            for (int j = 0; j < COLUMNS; j++)
+                            {
+                                bool colWin = true;
+                                for (int i = 1; i < ROWS; i++)
+                                {
+                                    if (slots[i, j] != slots[0, j])
+                                    {
+                                        colWin = false;
+                                        break;
+                                    }
                                 }
 
-                                if (mainDiagonalWin || antiDiagonalWin)
+                                if (colWin)
                                 {
-                                    isWin = false;
+                                    isWin = true;
+                                    break;
                                 }
-                                Console.Write("Enter Money:");
-                                int Money = Console.Read();
-                                while (Money < 50)
-                                {
-                                    Console.WriteLine($"Current cash value: {Money}");
-                                    Money--; 
-                                }
-                                if (Money > 1)
-                                    Console.WriteLine($"${Money-8}");
                             }
+                        }
+                        else if (choice == DIAGONAL_MODE)
+                        {
+                            bool mainDiagonalWin = true;
+                            for (int i = 1; i < ROWS; i++)
+                            {
+                                if (slots[i, i] != slots[0, 0])
+                                {
+                                    mainDiagonalWin = false;
+                                    break;
+                                }
+                            }
+
+                            bool antiDiagonalWin = true;
+                            for (int i = 1; i < ROWS; i++)
+                            {
+                                if (slots[i, COLUMNS - 1 - i] != slots[0, COLUMNS - 1])
+                                {
+                                    antiDiagonalWin = false;
+                                    break;
+                                }
+                            }
+
+                            if (mainDiagonalWin || antiDiagonalWin)
+                            {
+                                isWin = true;
+                                Console.WriteLine("🎉 Congratulations, you win!");
+                            }
+
+                            if (mainDiagonalWin || antiDiagonalWin)
+                            {
+                                isWin = false;
+                            }
+                        }
+
+                        if (!isWin)
+                        {
                             Console.WriteLine("Sorry, you  don't win!");
                             Console.Write($"Do you want to run again? Press {RUN_AGAIN}" + "     ");
                             Console.ReadLine();
                         }
+                        else
+                        {
+                            Money = Money + WIN_VAR;
+                        }
+
+                        Console.WriteLine("Do you want to play again? Y for yes N for no");
+                        string answer = Console.ReadLine();
+                        if (answer == "Y")
+                        {
+                            runAgain = true;
+                        }
+                        else
+                        {
+                            break;
+                        }
                     }
                 }
-            } //test 
+            }
+            
         }
     }
 }
